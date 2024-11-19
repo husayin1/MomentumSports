e355df7c83cb21869bc8dcd33cde80df9edd0ab2
Al-Hussein Abdulaziz <102304230+husayin1@users.noreply.github.com>
2024-11-18 16:17:41 -0800

    initial project

---
+++ Football League Application/Football League Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Football League Application
+//
+//  Created by husayn on 18/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
